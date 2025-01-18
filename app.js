1f1846e3c8e34e8fda9250aee251e5fcdb1e6cbe
kbrsrkl1 <kubranur216@gmail.com>
2025-01-19 01:20:49 +0300

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const btnEl = document.getElementById("btn");
+
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img")
+
+const animeNameEl = document.querySelector("anime-name")
+
+btnEl.addEventListener("click", function(){
+    try {
+        const response  =  fetch("")
+    } catch (error) {
+        
+    }
+})
