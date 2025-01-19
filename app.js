33ef17a566299e04aad6c86112cadf767cf75c30
kbrsrkl1 <kubranur216@gmail.com>
2025-01-19 13:56:47 +0300

    commit

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const btnEl = document.getElementById("btn");
 const animeContainerEl = document.querySelector(".anime-container");
 const animeImgEl = document.querySelector(".anime-img")
 
-const animeNameEl = document.querySelector("anime-name")
+const animeNameEl = document.querySelector(".anime-name")
+const animeUrl = "https://i.waifu.pics/MxWWsnO.jpeg";
+const apiUrl = `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(animeUrl)}&limit=1`;
 
-btnEl.addEventListener("click", function(){
+
+btnEl.addEventListener("click", async function(){
     try {
-        const response  =  fetch("")
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading..."
+        animeNameEl.innerText = "Updating..."
+        animeImgEl.src = "eclipse.svg"
+        const randomAnimeId = Math.floor(Math.random() * 500)
+        const apiUrl = `https://api.jikan.moe/v4/anime/${randomAnimeId}`;
+        const response  =  await fetch(apiUrl);
+        const data = await response.json();
+        animeContainerEl.style.display = "block";
+        animeImgEl.src = data.data.images.jpg.image_url;
+        animeNameEl.innerText = data.data.title;
+        btnEl.disabled = false;
+        btnEl.innerText = "Get Anime"
     } catch (error) {
-        
+        animeNameEl.innerText = "An error happened, please try again"        
+        btnEl.disabled = false;
+        btnEl.innerText = "Get Anime"
     }
 })
